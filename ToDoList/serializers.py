a594659cf91b22237fd0e4d8da860e323fa8697a
simonlee6969 <simonlee190301@gmail.com>
2024-07-26 14:29:25 +0800

    added ModelSerializer for TaskList

--- ToDoList/serializers.py
+++ ToDoList/serializers.py
@@ -5,3 +5,8 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = '__all__'
+        
+class TaskListSerilaizer(serializers.ModelSerializer):
+    class Meta:
+        model = TaskList
+        fields = '__all__'
