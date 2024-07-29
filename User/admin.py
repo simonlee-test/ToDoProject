44afb7e9f96a37f0115f71f927ee0ce70d9e87b4
simonlee6969 <simonlee190301@gmail.com>
2024-07-29 10:43:08 +0800

    added useradmin and database

--- User/admin.py
+++ User/admin.py
@@ -1,10 +1,10 @@
-# from django.contrib import admin
-# from django.contrib.auth.admin import UserAdmin
-# from .models import User
-# # Register your models here.
-# class CustomUserAdmin(UserAdmin):
-#     list_display = ('username', 'is_active', 'is_staff')
-#     list_filter = ('is_active', 'is_staff')
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'is_active', 'is_staff')
+    list_filter = ('is_active', 'is_staff')
 
 
-# admin.site.register(User, CustomUserAdmin)
+admin.site.register(User, CustomUserAdmin)
