d9609d3bdd3bd4230dd29876c43209ef635d904c
simonlee6969 <simonlee190301@gmail.com>
2024-07-29 10:22:25 +0800

    created User model

---
+++ User/admin.py
@@ -0,0 +1,10 @@
+# from django.contrib import admin
+# from django.contrib.auth.admin import UserAdmin
+# from .models import User
+# # Register your models here.
+# class CustomUserAdmin(UserAdmin):
+#     list_display = ('username', 'is_active', 'is_staff')
+#     list_filter = ('is_active', 'is_staff')
+
+
+# admin.site.register(User, CustomUserAdmin)
