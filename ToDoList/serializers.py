3ed0cbd5a16c4b6e0f816a6b00088063aa91cbf5
simonlee6969 <simonlee190301@gmail.com>
2024-07-30 17:19:02 +0800

    added checking logic for TaskSerializer where only the owner can add task the tasklists he owns

--- ToDoList/serializers.py
+++ ToDoList/serializers.py
@@ -1,13 +1,23 @@
 from rest_framework import serializers
 from ToDoList.models import Task, TaskList
-
+from rest_framework import request
 class TaskSerializer(serializers.ModelSerializer):
+    #this check ensure that only the owner can add tasks into the tasklist during POST and PUT request
+    def validate_tasklist(self, value):
+        """ 
+        Check that the task belongs to a tasklist of the current user.
+        """
+        if value not in self.context['request'].user.tasklists.all():
+            raise serializers.ValidationError('Only the owner of the tasklist can edit it.')
+        return value
+        
     class Meta:
         model = Task
         fields = '__all__'
         
 class TaskListSerilaizer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    #added extra_kwargs so that 'owner' field is no longer needed in api request.
     class Meta:
         model = TaskList
         fields = '__all__'
