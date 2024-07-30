51a3b92973dea16e06cc08490de6814c06be90e0
simonlee6969 <simonlee190301@gmail.com>
2024-07-30 09:51:44 +0800

    simplejwt implemented

--- ToDoApp/urls.py
+++ ToDoApp/urls.py
@@ -18,9 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('ToDoList.urls')),
+    path('api/token', TokenObtainPairView.as_view(), name = "token_obtain_pair"),
+    path('api/token/refresh', TokenRefreshView.as_view(), name = 'token_refresh'),
     
 ] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
